df4e4fb2fe4fca35fb74947a1e254892a99652bd
Kyle <k90armstrong@gmail.com>
2018-01-13 11:57:16 -0700

    fixes to passport and thr routes to enable authentication

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -8,23 +8,29 @@ var path = require("path");
 
 // Routes
 // =============================================================
-module.exports = function(app) {
+module.exports = function(app, passport) {
 
-  // Each of the below routes just handles the HTML page that the user gets sent to.
+    // Each of the below routes just handles the HTML page that the user gets sent to.
+    // index route loads landing.html
+    app.get("/", function(req, res) {
+        res.sendFile(path.join(__dirname, "../public/views/landing.html"));
+    });
 
-  // index route loads landing.html
-  app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/views/landing.html"));
-  });
-
-  // users homepage route loads home.html
-  app.get("/home", function(req, res) {
+    // users homepage route loads home.html
+    app.get("/home", isLoggedIn, function(req, res) {
     res.sendFile(path.join(__dirname, "../public/views/home.html"));
-  });
+    });
 
-  // childs profile page profile.html
-  app.get("/profile/:name", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/views/profile.html"));
-  });
+    // childs profile page profile.html
+    app.get("/profile/:name", isLoggedIn, function(req, res) {
+        res.sendFile(path.join(__dirname, "../public/views/profile.html"));
+    });
 
+    // middleware for logging in
+    function isLoggedIn(req, res, next) {
+        if (!req.isAuthenticated()) {
+            return next();    
+        }
+        res.redirect('/');
+    }
 };
