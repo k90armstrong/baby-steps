f0b34216607425a033db6f7a04a16ab623faaabb
pamula <prathima.amula@gmail.com>
2018-01-15 10:31:42 -0800

    added default.json file

--- models/child.js
+++ models/child.js
@@ -14,6 +14,36 @@ module.exports = function(sequelize, Sequelize) {
             type: Sequelize.STRING,
             notEmpty: true
         }
+        weight: {
+            type: Sequelize.DECIMAL,
+            allowNull: false
+
+        },
+        height: {
+            type:Sequelize.DECIMAL
+        },
+        hospitalborn:{
+            type: Sequelize.STRING
+        },
+        stateborn:{
+            type: Sequelize.STRING
+        }
+        category:{
+            type:Sequelize.STRING
+        }
+        birthdate:{
+            type:Sequelize.DATE
+        }
     });
+
+    Child.associate = function(models) {
+    // Associating Event with Posts
+    // When an Event is deleted, also delete any associated Posts
+    Child.hasMany(models.Event, {
+      onDelete: "cascade"
+    });
+  };
+     
+
     return Child;
 }   
--- models/event.js
+++ models/event.js
@@ -5,7 +5,41 @@ module.exports = function(sequelize, Sequelize) {
             autoIncrement: true,
             primaryKey: true,
             type: Sequelize.INTEGER
-        }
+        },
+        title:{
+        	type: Sequelize.STRING
+
+        },
+        imageurl:{
+        	type:Sequelize.STRING
+
+        },
+        description:{
+        	type:Sequelize.STRING
+
+        },
+        story:{
+        	type:Sequelize.STRING
+
+        },
+        date:{
+        	type:Sequelize.DATE
+
+        },
+        
+
+
     });
+    Event.associate = function(models) {
+    // We're saying that a child should belong to an Event
+    // A child can't be created without an Event due to the foreign key constraint
+    Event.belongsTo(models.Child, {
+      foreignKey: {
+        allowNull: false
+      }
+    });
+  };
+    
+
     return Event;
 }   
