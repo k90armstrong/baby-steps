cbc222727b73246a1afee512c14bc6b12fb0b166
pamula <prathima.amula@gmail.com>
2018-01-23 12:30:51 -0800

    heroku

--- models/index.js
+++ models/index.js
@@ -46,7 +46,7 @@ var env = process.env.NODE_ENV || "development";
 var config    = require(__dirname + '/../config/config.json')[env];
 
 
-var sequelize = new Sequelize(config.database, config.username, config.password, config);
+// var sequelize = new Sequelize(config.database, config.username, config.password, config);
 var db = {};
 
 if (config.use_env_variable) {
