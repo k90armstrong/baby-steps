320d4a01a6f9dd641c429ae77d493692869b69ab
Kyle <k90armstrong@gmail.com>
2018-03-03 11:51:59 -0700

    adds start of script to create default events

---
+++ baby-steps/server/scripts/addDefaultEventsToChild.js
@@ -0,0 +1,4 @@
+// this script will automatically run after a child is created
+// it will go through the default events and create them
+// for the child with the approximate date when the event will
+// occur
