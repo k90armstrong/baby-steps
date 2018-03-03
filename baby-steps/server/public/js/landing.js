052b4cf657fbf4bd2379de808a86427413148e78
Hannah Judd <Hannah@hannahs-air.uconnect.utah.edu>
2018-03-03 09:06:04 -0700

    all the front end is changed, will have LOTS of confilicts.

--- baby-steps/server/public/js/landing.js
+++ baby-steps/server/public/js/landing.js
@@ -1,3 +1,38 @@
 $(document).ready(function() {
     
+    
+    $('#nav').localScroll(800);
+    //.parallax(xPosition, speedFactor, outerHeight) options:
+    //xPosition - Horizontal position of the element
+    //inertia - speed to move relative to vertical scroll. Example: 0.1 is one tenth the speed of scrolling, 2 is twice the speed of scrolling
+    //outerHeight (true/false) - Whether or not jQuery should use it's outerHeight option to determine when a section is in the viewport
+    $('#intro').parallax("50%", 0.1);
+    $('#second').parallax("50%", 0.1);
+    $('.bg').parallax("50%", 0.4);
+    $('#third').parallax("50%", 0.3);
+
+    // this is for the LOGIN
+
+    var modal = document.getElementById('id01');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+// this is this SIGN UP
+
+var modal = document.getElementById('id02');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+
+
 });
