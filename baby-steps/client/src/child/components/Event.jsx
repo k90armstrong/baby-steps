4d3b77d67028463aa5b76fbc856523f84a90e1b2
pamula <prathima.amula@gmail.com>
2018-03-10 11:28:01 -0800

    added event.jsx file

---
+++ baby-steps/client/src/child/components/Event.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import moment from 'moment';
+class Event extends React.Component {
+    render() {
+        const date = moment(this.props.event.date).format("MMM Do YY");
+        return (
+            <div className="cd-timeline-block">
+                <div className="cd-timeline-image cd-picture">
+                    <img src={"this.props.event."} />
+                </div>
+                <div className="cd-timeline-content">
+                    <h2> {this.props.event.title}</h2>
+                    <p>{this.props.event.story}</p>
+                    <span className="cd-date">{date}</span>
+                </div>
+            </div>
+
+        )
+    }
+
+}
+export default  Event;
