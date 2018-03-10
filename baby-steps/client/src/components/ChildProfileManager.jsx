bae8894236c91bce9b19f3597f66df53c8470edd
Hannah Judd <Hannah@hannahs-air.uconnect.utah.edu>
2018-03-10 11:22:27 -0700

    added the profile page

--- baby-steps/client/src/components/ChildProfileManager.jsx
+++ baby-steps/client/src/components/ChildProfileManager.jsx
@@ -5,6 +5,7 @@ import BAppBar from '../components/BAppBar';
 import { loadedUser } from '../user/actions';
 import { connect } from 'react-redux';
 import { api } from '../api';
+import SingleChildProfile from '../components/SingleChildProfile';
 
 class ChildProfileManager extends React.Component {
 
@@ -15,6 +16,7 @@ class ChildProfileManager extends React.Component {
           accountClicked={()=>this.props.history.push('/app/myaccount')}
           logoutSuccess={this.props.loadedUser}
         />
+        <SingleChildProfile/>
       </div>
     );
   }
